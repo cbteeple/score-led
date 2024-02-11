aa32210d861f03673717ce9a6cac03ec6da1ec44
Clark Teeple <cbteeple@gmail.com>
2024-02-10 20:00:23 -0500

    switched to using addressable LED strips

---
+++ led_driver_neopixel.py
@@ -0,0 +1,174 @@
+import RPi.GPIO as GPIO
+import time
+import board
+import neopixel
+
+
+
+
+def init():
+	GPIO.setmode(GPIO.BCM)
+
+class PixelHandler:
+
+	PIN_MAP={
+		10: board.D10,
+		12: board.D12,
+		18: board.D18,
+		21: board.D21,
+	}
+
+	ORDER_MAP = {
+		'GRB': neopixel.GRB,
+		'RGB': neopixel.RGB,
+	}
+
+	# Pixel setup
+	def __init__(self, pin_configs, order='GRB'):
+		self.pin_configs = pin_configs
+		pixel_groups={}
+		for config in pin_configs:
+			num_pixels = config['num_pixels']
+
+			if pixel_groups.get(config['pin']):
+				pixel_groups[config['pin']]+=num_pixels
+			else:
+				pixel_groups[config['pin']]=num_pixels
+
+		self.pixel_groups = {}
+		for pin, num_pixels in pixel_groups.items():
+			pixels = neopixel.NeoPixel(
+				self.PIN_MAP[pin],
+				num_pixels,
+				brightness=0.2,
+				auto_write=False,
+				pixel_order=self.ORDER_MAP[order],
+			)
+			self.pixel_groups[pin] = pixels
+
+	def stop(self):
+		pass
+
+	def show_group(self, group_idx):
+		group_config = self.pin_configs[group_idx]
+		pixels = self.pixel_groups[group_config['pin']]
+		pixels.show()
+
+	def show_all(self):
+		for pixels in self.pixel_groups:
+			pixels.show()
+	
+	def set_color(self, group_idx, color, show=True):
+		print("Setting group %d to: %d, %d, %d"%(
+			group_idx,
+			color[0],
+			color[1],
+			color[2],
+		))
+
+		group_config = self.pin_configs[group_idx]
+		pixels = self.pixel_groups[group_config['pin']]
+		offset = group_config['start_idx']
+
+		for i in range(group_config['num_pixels']):
+			pixels[i+offset] = color
+
+		if show:
+			self.show_group(group_idx)
+		
+	def set_all_colors(self, color_vec, show=True):
+		print("Setting all groups at once.")
+
+		for i, color in enumerate(color_vec):
+			self.setColor(i, color, show=False)
+		
+		if show:
+			self.show_all()
+			
+	def set_all_colors_fade(
+			self,
+			new_color_vec,
+			cur_color_vec,
+			time_tot,
+			timestep=0.025,
+		):
+		ramps= []
+		num_steps=int(time_tot/timestep)
+		
+		#Precompute all color ramps
+		for new_color, curr_color in zip(new_color_vec, cur_color_vec):
+			fade_list = get_ramp(new_color, curr_color, num_steps)
+			ramps.append(fade_list)
+					
+		#Perform the fade
+		for step in range(num_steps+1):
+			color_vec=[]
+			for ramp in ramps:
+				color_vec.append(ramp[step])
+			self.set_all_colors(self, color_vec, show=True)
+			time.sleep(timestep)
+
+	
+#Create a long array with ramps of duty cycle values
+def get_ramp(
+		new_color_all,
+		cur_color_all,
+		num_steps,
+	):
+	
+	fadeList = [[0.0 for x in range(num_steps+1)] for y in range(len(new_color_all))]
+	
+	#For each color pin
+	for pinIdx, _ in enumerate(new_color_all):
+		#find the increment value
+		color_inc = (float(new_color_all[pinIdx]) - float(cur_color_all[pinIdx]))/float(num_steps)
+		
+		#For each timestep, store the value of the duty cycle
+		fadeList[pinIdx][0]= float(cur_color_all[pinIdx])
+		for stepIdx in range(1,num_steps+1):
+			fadeList[pinIdx][stepIdx] = fadeList[pinIdx][stepIdx-1]+color_inc
+			
+			#Make sure the duty cycle values are between 0 and 100
+			if fadeList[pinIdx][stepIdx]>100:
+				fadeList[pinIdx][stepIdx]=100
+				
+			if fadeList[pinIdx][stepIdx]<0:
+				fadeList[pinIdx][stepIdx]=0
+				
+	for pinIdx in range(len(new_color_all)):
+		fadeList[pinIdx][num_steps]=float(new_color_all[pinIdx])
+				
+	return fadeList
+	
+
+	
+	
+
+def clearColors(pins):
+	GPIO.setmode(GPIO.BCM)
+	for i, pin in enumerate(pins):
+		GPIO.setup(pin, GPIO.OUT)
+		GPIO.output(pin, GPIO.LOW)
+		
+def setColors(pins):
+	GPIO.setmode(GPIO.BCM)
+	for i, pin in enumerate(pins):
+		GPIO.setup(pin, GPIO.OUT)
+		GPIO.output(pin, GPIO.HIGH)
+
+
+
+#For Testing Purposes
+	
+# PWM
+def runPulse(pwm):
+	for dc in range(0, 101, 5):
+		for p in pwm:
+			print p
+			p.ChangeDutyCycle(dc)
+		time.sleep(0.03)
+	for dc in range(100, -1, -5):
+		for p in pwm:
+			p.ChangeDutyCycle(dc)
+		time.sleep(0.03)
+	return pwm
---
+++ main_multithreading2024.py
@@ -0,0 +1,355 @@
+#Get functions from external sources
+import threading
+from hex2rgb import hex2rgb, rgb2hex
+import nfl_scores
+from twitter_sentiment import TwitterClient
+from collections import deque
+import time
+import random
+import math
+
+import led_driver_neopixel as led
+	
+#Define some low-level pin variables
+pin_config = [
+    {'pin': 10, 'num_pixels': 15, 'start_idx': 0},
+	{'pin': 10, 'num_pixels': 22, 'start_idx': 15},
+	{'pin': 12, 'num_pixels': 15, 'start_idx': 0},
+	{'pin': 12, 'num_pixels': 22, 'start_idx': 15},
+]
+
+skipIntro=1
+
+#Start with a color (will change this eventually to start with #000000)
+
+#Eagles
+team2_bright = "#00cc2a"
+team2_color  = "#005b12"
+team2_dim    = "#002808"
+
+#Patriots
+team1_bright = "#0031e8"
+team1_color  = "#001875"
+team1_dim    = "#000b35"
+
+color_vec=[hex2rgb(team1_color), hex2rgb("#000000"), hex2rgb(team2_color), hex2rgb("#000000")]
+
+fadeTime_default = 0.25
+pulseTime = 3.5
+pulseFlag=0
+singlePulse=0;
+
+team1_pulse=[team1_bright, team1_dim]
+team2_pulse=[team2_bright, team2_dim]
+
+
+
+
+
+
+
+# Collect the tweets!
+
+# How many past twitter api calls to save
+save_num_calls = 4
+num_tweets = 400
+call_rate = 15  # seconds
+
+team1 = 'NE'
+team2 = 'PHI'
+
+tags = {
+    team1: '#Patriots',
+    team2: '#Eagles'
+}
+
+# Keep an array of the # of tweets sentiment
+# [ positive, negative, neutral ]
+tweet_sentiment = {
+    team1: deque([]),
+    team2: deque([])
+}
+
+# creating object of TwitterClient Class
+api = TwitterClient()
+
+
+
+def new_analysis(tag):
+    # calling function to get tweets
+    tweets = api.get_tweets(query = tag, count = num_tweets) 
+    # Number of positive tweets from tweets
+    pos_tweets = len([tweet for tweet in tweets if tweet['sentiment'] == 'positive'])
+    # Number of negative tweets from tweets
+    neg_tweets = len([tweet for tweet in tweets if tweet['sentiment'] == 'negative'])
+    # Number of neutral tweets
+    neut_tweets = len(tweets) - pos_tweets - neg_tweets 
+
+    return pos_tweets, neg_tweets, neut_tweets
+
+
+
+
+
+
+
+
+
+
+#Define the function that will become our led loom process
+def ledLoop(led_handler, goFlag, updateFlag):
+	#Setup all the groups of LED pins and begin PWM on them
+	led_handler.setAllColors(color_vec)
+	
+	curr_colors=color_vec[:][:]
+	#Big loop - this is where colors actually get set
+	trans_time=fadeTime_default
+	
+	while not goFlag.isSet():
+		#led.setAllColors(pwm_vec,color_vec)
+		#for i, pwm in enumerate(pwm_vec):
+		#	led.setColor(pwm,color_vec[i])
+		if pulseFlag:
+			trans_time=pulseTime
+		else:
+			trans_time=fadeTime_default
+			
+		
+		if updateFlag.isSet():
+			led_handler.set_all_colors_fade(color_vec, curr_colors, trans_time)
+			curr_colors=color_vec[:][:]
+			updateFlag.clear()
+		time.sleep(0.1)
+	
+	trans_time=fadeTime_default	
+	led_handler.set_all_colors_fade(
+		[[0 for i in range(3)] for j in range(4)],
+		curr_colors,
+		trans_time
+	)
+	time.sleep(fadeTime_default+1.0)
+	
+	#Once the loop is broken, we stop PWM and clean up the GPIO ports
+	led_handler.stop()
+
+#Run Stuff
+if __name__ == '__main__':
+	#Start a new process that pulses the intensity of the LEDs
+	led_handler = led.PixelHandler(pin_config)
+	
+	goFlag=threading.Event()
+	updateFlag = threading.Event()
+	ledUpdate = threading.Thread(
+		name='non-block',
+		target=ledLoop,
+		args=(led_handler,
+		    goFlag,
+			updateFlag
+			))
+	ledUpdate.start()
+	
+	#Change the color of some of the LED's at a test
+	if not skipIntro:
+		color_vec[0]=hex2rgb(team1_color)
+		color_vec[1]=hex2rgb("#FF0000")
+		color_vec[2]=hex2rgb(team2_color)
+		color_vec[3]=hex2rgb("#00FF00")
+		updateFlag.set()
+		time.sleep(fadeTime_default+1.0)
+	
+		color_vec[0]=hex2rgb(team1_color)
+		color_vec[1]=hex2rgb("#00FF00")
+		color_vec[2]=hex2rgb(team2_color)
+		color_vec[3]=hex2rgb("#FF0000")
+		updateFlag.set()
+		time.sleep(fadeTime_default+1.0)
+
+		color_vec[0]=hex2rgb("#000000")
+		color_vec[1]=hex2rgb("#000000")
+		color_vec[2]=hex2rgb("#000000")
+		color_vec[3]=hex2rgb("#000000")
+		updateFlag.set()
+		time.sleep(fadeTime_default+1.0)
+	
+	
+		color_vec[0]=hex2rgb(team1_color)
+		color_vec[1]=hex2rgb("#000000")
+		color_vec[2]=hex2rgb(team2_color)
+		color_vec[3]=hex2rgb("#000000")
+		updateFlag.set()
+		time.sleep(fadeTime_default+1.0)
+	
+	pulseFlag=1
+	
+	
+	try:
+		positivity = {}
+		while 1:	
+			#Check at regular intervals for the twitter sentiment
+			for team, vals in tweet_sentiment.iteritems():
+				new_tweets = new_analysis(tags[team])
+				tweet_sentiment[team].append(new_tweets)
+
+				if len(tweet_sentiment[team]) > save_num_calls:
+					tweet_sentiment[team].popleft()
+
+				# Get the percentage of positive/negative/neutral tweets in the list
+				t_s = tweet_sentiment[team]
+				pos = sum([t[0] for t in t_s])
+				neg = sum([t[1] for t in t_s])
+				neut = sum([t[2] for t in t_s])
+
+				positivity[team] =  100 * float(pos) / (pos + neg)
+				
+				#positivity[team]=random.randint(0,100)
+				print team + ': ' + str(positivity[team])
+			
+			
+			#find the R and G values (B is always 0)
+			
+			R=25.5*math.sqrt(-positivity[team1]+100)
+			G=255./10000*(positivity[team1]**2)
+			B=0
+			
+			color_vec[1]= [R,G,B]
+			
+			R=25.5*math.sqrt(-positivity[team2]+100)
+			G=255./10000*(positivity[team2]**2)
+			B=0
+			color_vec[3]= [R,G,B]
+			
+			
+			
+			#Check at regular intervals for the score
+			try:
+				game_data = nfl_scores.get_scores()
+				team1_data = nfl_scores.get_team_data(game_data, team1)
+				team2_data = nfl_scores.get_team_data(game_data, team2)
+			
+				team1_score=nfl_scores.get_score(team1_data)
+				team2_score=nfl_scores.get_score(team2_data)
+				
+				team1_posession = nfl_scores.has_possession(game_data,team1)
+				team2_posession = nfl_scores.has_possession(game_data,team2)
+				
+				qtr=nfl_scores.get_qtr(game_data)
+			
+				#Score to RGB
+				#print team1_score, team2_score
+				#print
+				if qtr == "Pregame":
+					team1_pulse=[team1_bright, team1_dim]
+					team2_pulse=[team2_bright, team2_dim]
+					pulseFlag=1;
+					singlePulse=0
+					pulseTime=3.5
+					
+					
+				else:
+					if team1_score is not None and team2_score is not None:
+						print(team1_score)
+						print(team2_score)
+						if team1_score > team2_score:
+							color_vec[0]=hex2rgb(team1_bright)
+							color_vec[2]=hex2rgb(team2_dim)
+						elif team1_score < team2_score:
+							color_vec[0]=hex2rgb(team1_dim)
+							color_vec[2]=hex2rgb(team2_bright)
+						else:
+							color_vec[0]=hex2rgb(team1_color)
+							color_vec[2]=hex2rgb(team2_color)
+					
+						pulseFlag=0
+											
+					else:
+						pulseFlag=1;
+						singlePulse=0
+						pulseTime=3.5
+						print('scores were null')
+					
+
+					print(color_vec[0])
+					print(color_vec[2])
+					'''if team1_posession:
+						print('team1 has posession')
+						pulseFlag=1
+						singlePulse=1
+						pulseTime=1.0
+						rgb=[0, 0, 0]
+						for idx in range(len(rgb)):
+							rgb[idx]= int(color_vec[0][idx] + color_vec[0][idx]*0.5)
+											
+						team1_pulse=[rgb2hex(color_vec[0]),rgb2hex(rgb)]
+						team2_pulse=[rgb2hex(color_vec[2]),rgb2hex(color_vec[2])]
+					
+					
+					elif team2_posession:
+						print('team2 has posession')
+						pulseFlag=1
+						singlePulse=1
+						pulseTime=1.0
+						rgb=[0, 0, 0]
+						for idx in range(len(rgb)):
+							rgb[idx]= int(color_vec[2][idx] + color_vec[2][idx]*0.5)
+											
+						team1_pulse=[rgb2hex(color_vec[0]),rgb2hex(color_vec[0])]
+						team2_pulse=[rgb2hex(color_vec[2]),rgb2hex(rgb)]
+					'''
+					
+			except TypeError:
+				pass					
+
+				
+			if pulseFlag:
+				#Set color for 4 sec, 4 times
+				
+				if not singlePulse:
+					numPulses=int(math.ceil(15/(pulseTime*2)))
+					deadTime=0.0
+				else:
+					numPulses=1
+					deadTime=15-pulseTime*2
+					
+				
+				for idx in range(numPulses):
+					color_vec[0]=hex2rgb(team1_pulse[0])
+					color_vec[2]=hex2rgb(team2_pulse[0])
+					updateFlag.set()
+					time.sleep(pulseTime+0.25)
+					
+					
+					color_vec[0]=hex2rgb(team1_pulse[1])
+					color_vec[2]=hex2rgb(team2_pulse[1])
+					updateFlag.set()
+					time.sleep(pulseTime+0.25)
+					
+				time.sleep(deadTime)
+					
+					
+			else:				
+				color_temp=color_vec[:]
+				
+				
+				color_vec[1]=hex2rgb("7C7C7C")
+				color_vec[3]=hex2rgb("7C7C7C")
+				
+				updateFlag.set()
+				time.sleep(fadeTime_default+0.2)
+				
+				color_vec[1]=color_temp[1]
+				color_vec[3]=color_temp[3]
+				updateFlag.set()
+				#print nfl_scores.pretty_json(game_data)
+				time.sleep(call_rate-1)
+			
+			
+			
+	except KeyboardInterrupt:
+		pass
+
+	
+	
+	#Set the flag to end the PWM process
+	goFlag.set()
+	
+	
